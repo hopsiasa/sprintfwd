9dfdd3abaf6b43b7870bef75b049eeb64503d26b
Razvan Melnic <razvan.melnic@gmail.com>
2024-01-04 19:56:36 +0200

    Model specs

---
+++ database/seeders/MemberProjectSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Seeder;
+use App\Models\Member;
+
+class MemberProjectSeeder extends Seeder
+{
+    public function run()
+    {
+        // Attach projects to members
+        Member::all()->each(function ($member) {
+            $projects = Project::inRandomOrder()->limit(rand(1, 5))->get();
+            $member->projects()->attach($projects);
+        });
+    }
+}
