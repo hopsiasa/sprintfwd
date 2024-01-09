a952acb62f09a4c1a7ce08fd60654098370f2993
Razvan Melnic <razvan.melnic@gmail.com>
2024-01-09 11:42:50 +0200

    Forgot to show city, state and country inputs for members

--- resources/views/members/edit.blade.php
+++ resources/views/members/edit.blade.php
@@ -7,8 +7,19 @@
         @method('PUT')
         <label for="first_name">First Name:</label>
         <input type="text" name="first_name" value="{{ $member->first_name }}" required>
+        <br />
         <label for="last_name">Last Name:</label>
         <input type="text" name="last_name" value="{{ $member->last_name }}" required>
+        <br />
+        <label for="city">City:</label>
+        <input type="text" name="city" value="{{ $member->city }}" required>
+        <br />
+        <label for="state">State:</label>
+        <input type="text" name="state" value="{{ $member->state }}" required>
+        <br />
+        <label for="country">Country:</label>
+        <input type="text" name="country" value="{{ $member->country }}" required>
+        <br />
         <label for="team">Team:</label>
         <select name="team_id" id="team">
             @foreach($teams as $team)
@@ -20,6 +31,7 @@
                 </option>
             @endforeach
         </select>
+        <br />
         <button type="submit">Update Member</button>
     </form>
 @endsection
--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $member->first_name }} {{ $member->last_name }}</h1>
+    <h1>Member: {{ $member->first_name }} {{ $member->last_name }}</h1>
     <h3>City: {{ $member->city }}</h3>
     <h3>State: {{ $member->state }}</h3>
     <h3>Country: {{ $member->country }}</h3>
